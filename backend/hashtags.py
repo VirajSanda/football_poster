b95b52bddacabbadcca77b2adb4858aec441fe76
Viraj Sandaruwan <virajsandaruwan92@gmail.com>
2025-11-22 22:49:42 +0800

    removed unused

--- backend/hashtags.py
+++
@@ -1,9 +0,0 @@
-def generate_hashtags(title: str, summary: str = ""):
-    """Generate up to 6 unique hashtags."""
-    words = (title + " " + summary).split()
-    hashtags = []
-    for w in words:
-        clean = "".join(c for c in w if c.isalnum())
-        if len(clean) > 3:
-            hashtags.append(f"#{clean.capitalize()}")
-    return list(dict.fromkeys(hashtags))[:6]
